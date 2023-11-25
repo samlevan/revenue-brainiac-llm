291acd08e69332690b749b431e41e2f5d1ca62fe
Sam Levan <samlevan@users.noreply.github.com>
2023-11-25 17:14:23 +0000

    ...

--- Hello.py
+++ Hello.py
@@ -1,10 +1,24 @@
 import streamlit as st
 from streamlit.logger import get_logger
 import superpowered
+import os
+
+# Initialize API key and secret
+os.environ["SUPERPOWERED_API_KEY_ID"] = st.secrets.SUPERPOWERED_API_KEY
+os.environ["SUPERPOWERED_API_KEY_SECRET"]= st.secrets.SUPERPOWERED_SECRET_KEY
 
 LOGGER = get_logger(__name__)
 
-print(st.secrets)
+def chat(prompt):
+    # Make the request for the desired chat thread and message
+    response = superpowered.get_chat_response(
+        thread_id="c2fc2461-5377-4a90-8a4a-579ae40aae3f",
+        input=prompt
+    )
+
+    # Print the response
+    return response["interaction"]["model_response"]["content"]
+
 
 def run():
     st.set_page_config(
@@ -12,11 +26,11 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Revenue Brainiac LLM! 👋")
+    st.write("# Hi, I am Graham, your LLM GTM expert 👋")
 
     st.markdown(
         """
-Hi, I'm a chatbot who have read every single blog post from MadKudu and OpenView.
+I have read every single blog post from MadKudu and OpenView.
 I know a lot about GTM strategy, product-led growth, ABM, sales playbooks, etc.
     """
     )
@@ -30,36 +44,18 @@ I know a lot about GTM strategy, product-led growth, ABM, sales playbooks, etc.
         st.chat_message(msg["role"]).write(msg["content"])
 
 
-# # Initialize API key and secret
-# os.environ["SUPERPOWERED_API_KEY_ID"]="INSERT_API_KEY_ID_HERE"
-# os.environ["SUPERPOWERED_API_KEY_SECRET"]="INSERT_API_KEY_SECRET_HERE"
-
-# # Make the request for the desired chat thread and message
-# response = superpowered.get_chat_response(
-#     thread_id="c2fc2461-5377-4a90-8a4a-579ae40aae3f",
-#     input="",
-# )
-
-# # Print the response
-# print (response["interaction"]["model_response"]["content"])
-
     if prompt := st.chat_input(placeholder="How do you make ABM and PLG work together?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.chat_message("user").write(prompt)
-        st.chat_message("assistant").write("<a>yoyo</a>")
+        response = chat(prompt)
+        st.chat_message("assistant").write(response)
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
-        # if not openai_api_key:
-        #     st.info("Please add your OpenAI API key to continue.")
-        #     st.stop()
-
-        # llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
-        # search = DuckDuckGoSearchRun(name="Search")
-        # search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
         # with st.chat_message("assistant"):
-        #     st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        #     response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
-        #     st.session_state.messages.append({"role": "assistant", "content": response})
-        #     st.write(response)
+            # st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            # response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+            # st.session_state.messages.append({"role": "assistant", "content": response})
+            # st.write(response)
 
 
 if __name__ == "__main__":
